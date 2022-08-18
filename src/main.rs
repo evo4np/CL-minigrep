21c6d6823faf0f99b29621c147b6e8e7963b80bc
evo4np <anilpdlx@gmail.com>
2022-08-18 14:46:09 +0200

    Creating a Constructor for Config

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -17,10 +17,12 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone(); // makes full copy of the data fo the config instance to own
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone(); // makes full copy of the data fo the config instance to own
     let file_path = args[2].clone();
 
     Config {query, file_path}
-
+    }
 }
+
